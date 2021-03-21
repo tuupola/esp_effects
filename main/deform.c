360170acbe4a1e24eb9bef7a7212fc4154ccf9a0
Mika Tuupola <tuupola@appelsiini.net>
2021-03-21 17:29:50 +0200

    Add some consts to help compiler optimize

--- main/deform.c
+++ main/deform.c
@@ -50,45 +50,45 @@ void deform_init()
     for (uint16_t j = 0; j < DISPLAY_HEIGHT; j += PIXEL_SIZE) {
         for (uint16_t i = 0; i < DISPLAY_WIDTH; i += PIXEL_SIZE) {
 
-            const float x = -1.00f + 2.00f * (float)i / (float)DISPLAY_WIDTH;
-            const float y = -1.00f + 2.00f * (float)j / (float)DISPLAY_HEIGHT;
+            const float x = -1.00f + 2.00f * i / DISPLAY_WIDTH;
+            const float y = -1.00f + 2.00f * j / DISPLAY_HEIGHT;
             const float r = sqrtf(x * x + y * y);
             const float a = atan2f(y, x);
 
-            float u = cosf(a) / r;
-            float v = sinf(a) / r;
+            const float u = cosf(a) / r;
+            const float v = sinf(a) / r;
 
-            // float u = 0.5 * a / M_PI;
-            // float v = sin(7 * r);
+            // const float u = 0.5 * a / M_PI;
+            // const float v = sin(7 * r);
 
-            // float u = 0.02 * y + 0.03 * cos(a * 3) / r;
-            // float v = 0.02 * x + 0.03 * sin(a * 3) / r;
+            // const float u = 0.02 * y + 0.03 * cos(a * 3) / r;
+            // const float v = 0.02 * x + 0.03 * sin(a * 3) / r;
 
-            // float u = 1 / (r + 0.5 + 0.5 * sin(5 * a));
-            // float v = a * 3 / M_PI;
+            // const float u = 1 / (r + 0.5 + 0.5 * sin(5 * a));
+            // const float v = a * 3 / M_PI;
 
 
-            // float u = x * cos(2 * r) - y * sin(2 * r);
-            // float v = y * cos(2 * r) + x * sin(2 * r);
+            // const float u = x * cos(2 * r) - y * sin(2 * r);
+            // const float v = y * cos(2 * r) + x * sin(2 * r);
 
-            // float u = 0.3 / (r + 0.5 * x);
-            // float v = 3 * a / M_PI;
+            // const float u = 0.3 / (r + 0.5 * x);
+            // const float v = 3 * a / M_PI;
 
-            // float u = 0.1 * x / (0.11 + r * 0.5);
-            // float v = 0.1 * y / (0.11 + r * 0.5);
+            // const float u = 0.1 * x / (0.11 + r * 0.5);
+            // const float v = 0.1 * y / (0.11 + r * 0.5);
 
-            // float u = r * cos(a + r);
-            // float v = r * sin(a + r);
+            // const float u = r * cos(a + r);
+            // const float v = r * sin(a + r);
 
-            // float u = x / fabs(y);
-            // float v = 1 / fabs(y);
+            // const float u = x / fabs(y);
+            // const float v = 1 / fabs(y);
 
-            // float u = x;
-            // float v = y;
+            // const float u = x;
+            // const float v = y;
 
             /* Target x and y coordinates in the texture. */
-            int8_t tx = ((int8_t)(HEAD_WIDTH * u)) % HEAD_WIDTH;
-            int8_t ty = ((int8_t)(HEAD_HEIGHT * v)) % HEAD_HEIGHT;
+            const int8_t tx = ((int8_t)(HEAD_WIDTH * u)) % HEAD_WIDTH;
+            const int8_t ty = ((int8_t)(HEAD_HEIGHT * v)) % HEAD_HEIGHT;
 
             *(ptr++) = tx;
             *(ptr++) = ty;
@@ -111,7 +111,7 @@ void deform_render()
             v = abs(v) % HEAD_HEIGHT;
 
             /* Get the pixel from texture and put it to the screen. */
-            color_t *color = (color_t*) (head + HEAD_WIDTH * sizeof(color_t) * v + sizeof(color_t) * u);
+            const color_t *color = (color_t*) (head + HEAD_WIDTH * sizeof(color_t) * v + sizeof(color_t) * u);
 
             if (1 == PIXEL_SIZE) {
                 hagl_put_pixel(x, y, *color);
